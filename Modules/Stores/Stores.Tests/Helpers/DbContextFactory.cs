5f353e87e3adda9748cb4b9941cba511dae8d633
Otakukorean <BBoomstation1@gmail.com>
2026-04-24 21:32:50 +0300

    test: add CreateStore handler tests (8 cases)

--- Modules/Stores/Stores.Tests/Helpers/DbContextFactory.cs
+++ Modules/Stores/Stores.Tests/Helpers/DbContextFactory.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Stores.Data;
+
 namespace Stores.Tests.Helpers;
 
-public class DbContextFactory
+public static class DbContextFactory
 {
-    
+    public static StoresDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<StoresDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        
+        var context = new StoresDbContext(options);
+        context.Database.EnsureCreated();
+        return context;
+    }
 }
